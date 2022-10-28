6d02a6ff976f2f2ac05569da4a8165a85c21a7a4
zakken <matheus@marcogabriel.com.br>
2022-10-27 22:21:56 -0300

    Created a functionality to read the indicator of bits and get the MaskKey and decoded the buffer of bytes

--- server.mjs
+++ server.mjs
@@ -7,6 +7,9 @@ const WEBSOCKET_MAGIC_STRING_KEY = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
 const SEVEN_BITS_MARKER = 125
 const SIXTEEN_BITS_MARKER = 126
 const SIXTYTEEN_BITS_MARKER = 127
+const FIRST_BIT = 128
+
+const MASK_KEY_BYTES_LENGTH = 4
 
 const server = createServer((req, res) => {
     res.writeHead(200)
@@ -22,7 +25,7 @@ function onSocketUpgrade(req, socket, head) {
     const headers = prepareHandshakeHeaders(socketKey);
 
     socket.write(headers);
-    socket.on('readable', () => onSocketUpgrade(socket))
+    socket.on('readable', () => onSocketReadable(socket))
 }
 
 function onSocketReadable(socket) {
@@ -30,7 +33,33 @@ function onSocketReadable(socket) {
     // 1 - 1 byte
     socket.read(1);
 
-    socket.read(1)
+    const [markerAndPayloadLength] = socket.read(1)
+    // Beccause the first bit is always 1 for client-to-server messages
+    // you can subtract one bitr (128 or 10000000)
+    // from this byte 
+    const lengthIndicatorInBits = markerAndPayloadLength - FIRST_BIT
+    let messageLength = 0
+
+    if(lengthIndicatorInBits <= SEVEN_BITS_MARKER) {
+        messageLength = lengthIndicatorInBits
+    } else {
+        throw new Error(`your message is too long! we don't handle 64-bit messages`)
+    }
+
+    const maskKey = socket.read(MASK_KEY_BYTES_LENGTH);
+    const encoded = socket.read(messageLength);
+    const decoded = unMask(encoded, maskKey);
+    console.log(decoded.toString())
+}
+
+function unMask(encodedBuffer, maskKey) {
+    const finalBuffer = Buffer.from(encodedBuffer);
+
+    for (let i = 0; i < encodedBuffer.length; i++) {
+        finalBuffer[i] = encodedBuffer[i] ^ maskKey[i % 4];
+    }
+
+    return finalBuffer;
 }
 
 function prepareHandshakeHeaders(id) {
