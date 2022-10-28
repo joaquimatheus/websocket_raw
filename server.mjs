4c3e74c762911510c65f29949da5fc21cb3d3201
zakken <matheus@marcogabriel.com.br>
2022-10-27 21:23:19 -0300

    finished the handshake

--- server.mjs
+++ server.mjs
@@ -1,5 +1,12 @@
-import { createServer } from 'http'
+import { createServer } from 'http';
+import crypto from 'crypto';
+
 const PORT = 1337
+const WEBSOCKET_MAGIC_STRING_KEY = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+
+const SEVEN_BITS_MARKER = 125
+const SIXTEEN_BITS_MARKER = 126
+const SIXTYTEEN_BITS_MARKER = 127
 
 const server = createServer((req, res) => {
     res.writeHead(200)
@@ -7,11 +14,43 @@ const server = createServer((req, res) => {
 })
 .listen(PORT, () => console.log('server listening to', PORT))
 
-server.on('upgrade', (req, socket, head) => {
-    console.log({ 
-        head: req.headers
-    })
-})
+server.on('upgrade', onSocketUpgrade )
+
+function onSocketUpgrade(req, socket, head) {
+    const { 'sec-websocket-key': socketKey } = req.headers;
+    console.log(`${socketKey} connected`);
+    const headers = prepareHandshakeHeaders(socketKey);
+
+    socket.write(headers);
+    socket.on('readable', () => onSocketUpgrade(socket))
+}
+
+function onSocketReadable(socket) {
+    // consume first bit
+    // 1 - 1 byte
+    socket.read(1);
+
+    socket.read(1)
+}
+
+function prepareHandshakeHeaders(id) {
+    const acceptKey = createSocketAccept(id)
+    const headers = [
+        'HTTP/1.1 101 Switching Protocols',
+        'Upgrade: websocket',
+        'Connection: Upgrade',
+        `Sec-WebSocket-Accept: ${acceptKey}`,
+        ''
+    ].map(line => line.concat('\r\n')).join('');
+
+    return headers;
+}
+
+function createSocketAccept(id) {
+    const sha1 = crypto.createHash('sha1')
+    sha1.update(id + WEBSOCKET_MAGIC_STRING_KEY)
+    return sha1.digest('base64')
+}
 
 function fatalHandler(err) {
     console.log(err, { FATA: true });
